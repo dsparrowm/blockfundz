645e934108c538318f67a42cd532c4764bcfbcda
Davies Aniefiok <daviesaniefiok32@gmail.com>
2024-12-26 23:08:45 +0100

    dockerized the backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,13 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
