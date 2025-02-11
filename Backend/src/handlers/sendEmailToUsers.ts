7e798532dfe1693676e1a44b97935a7f34176cdd
Davies Aniefiok <daviesaniefiok32@gmail.com>
2025-02-11 09:31:14 +0100

    fixed a bug in sendEmailToUsers handler that prevented it from sending mails

--- Backend/src/handlers/sendEmailToUsers.ts
+++ Backend/src/handlers/sendEmailToUsers.ts
@@ -1,16 +1,17 @@
 import sendEmail from "../helpers/sendEmail";
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 
-const sendEmailToUsers = async (req: Request, res:Response) => {
-    const {recipients, subject, body} = req.body;
+const sendEmailToUsers = async (req: Request, res: Response) => {
+    const { recipients, subject, body } = req.body;
     const mailOptions = {
         subject,
-        htmlContent: `<!DOCTYPE html><html><head><title>Apex Digital</title></head><body>${body}</body></html>`,
-        recipients,
+        from: 'support@nexgencrypto.live',
+        to: recipients,
+        html: `<!DOCTYPE html><html><head><title>Apex Digital</title></head><body>${body}</body></html>`,
     }
     try {
         await sendEmail(mailOptions);
-        res.status(200).json({message: 'Email sent successfully'});
+        res.status(200).json({ message: 'Email sent successfully' });
     } catch (error) {
         console.log(error);
     }
